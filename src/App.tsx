8db19d46f31790803180dc816fdb677e834a8367
AleksandrDeveloper <aleks777.chernof7@mail.ru>
2020-09-02 21:47:48 +0300

    d

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, createRef } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+type TypeProps={
+  title?:string
+}
+type typeState={
+  counter:number
+  focus:boolean
 }
 
-export default App;
+
+export default class App extends Component<TypeProps,typeState> {
+
+  constructor(props:TypeProps){
+    super(props)
+    this.state={
+      counter:0,
+      focus:false
+    }
+
+  }
+  private rootRef = createRef<HTMLButtonElement>()
+ 
+  componentDidMount():void{
+    console.log(this.rootRef.current);   
+     
+  } 
+  shouldComponentUpdate(nextProps:TypeProps,nextState:typeState):boolean{
+    return true 
+  }
+
+  handlerPlusCounter=(e:React.MouseEvent<HTMLButtonElement>)=>{
+    console.log(e.clientY);
+    
+    this.setState(prevState=>{
+      return {counter:prevState.counter+1}
+    })
+  }
+  hadlerInputElement=(e:React.FocusEvent<HTMLInputElement>)=>{
+    console.log(e.target);
+    
+  }
+  hadlerSubmit=(e:React.FormEvent<HTMLFormElement>)=>{
+    e.preventDefault()
+    console.log('Submit');
+    console.log(this.rootRef.current);  
+    
+  }
+  
+
+  render() {
+    return (
+      <div>
+        <input type="text"  /> 
+        <div >
+          dfdfsf
+        </div>
+        <h1>{this.state.counter}  </h1>
+ 
+        <button onClick={this.handlerPlusCounter} ref={this.rootRef}>+</button>
+        <h3>{this.props.title} </h3>
+
+        <form action="" onSubmit={this.hadlerSubmit}>
+          <input type="text" onFocus={this.hadlerInputElement}/>
+          <button type="submit">ff</button>
+        </form>
+      </div>
+    )
+  }
+}
